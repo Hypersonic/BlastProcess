eb34374e40f9a9421c9aafcbb2a12e8703ec8e19
Zane Sterling <sterling.zane@gmail.com>
2015-04-11 01:04:58 -0400

    Add simple window

--- source/app.d
+++ source/app.d
@@ -1,6 +1,34 @@
 import std.stdio;
 
+import gfm.sdl2;
+
+import state;
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	auto width  = 1000;
+	auto height = 1000;
+
+	GameState state;
+	state.sdl2 = new SDL2(null);
+	auto window = new SDL2Window(state.sdl2,
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		width, height,
+		SDL_WINDOW_SHOWN);
+	state.renderer = new SDL2Renderer(window);
+
+	// state loop
+	while (state.running) {
+		state.sdl2.processEvents();
+
+		checkKeys(state);
+	}
+
+	// ciao!
+	writeln("quitting; goodbye");
+}
+
+void checkKeys(ref GameState state) {
+	if (state.sdl2.keyboard().isPressed(SDLK_q))
+		state.running = false;
 }
---
+++ source/state.d
@@ -0,0 +1,7 @@
+import gfm.sdl2;
+
+struct GameState {
+	bool running = true;
+	SDL2 sdl2;
+	SDL2Renderer renderer;
+};
