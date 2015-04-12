643ff0de168e0d13431a74d3b278baf7a36d6c33
Zane Sterling <sterling.zane@gmail.com>
2015-04-11 21:40:47 -0400

    Get rainbow fixed

--- source/scene2.d
+++ source/scene2.d
@@ -1,5 +1,6 @@
 import std.math;
 import std.stdio;
+import std.algorithm;
 
 import derelict.sdl2.sdl;
 import derelict.opengl3.gl;
@@ -17,13 +18,23 @@ int[] tetraTriPtrs = [
 
 double tetraRot = 0;
 
-double[2][] rainbow = [
-	[-1000, 0],
-	[-200, 0]
+double[] rainbow = [
+	0, 0
 ];
+const double NUM_RB_SEGS = 60;
+const double RB_HEIGHT = 100;
+const double RB_AMPL = 100;
+const double SEG_W = 1000. / NUM_RB_SEGS;
+
+const double TOTAL_ANGLE = PI;
+double theta = TOTAL_ANGLE;
 
 void init(ref State state) {
 	tetraVerts = genTetraVerts();
+
+	foreach (i; 0..NUM_RB_SEGS) {
+		rainbow ~= RB_AMPL * sin(i / NUM_RB_SEGS * TOTAL_ANGLE);
+	}
 }
 
 double[] genTetraVerts() {
@@ -40,6 +51,12 @@ double[] genTetraVerts() {
 
 void update(ref State state) {
 	tetraRot += 0.03;
+
+	if (rainbow.length > 0) {
+		rainbow = rainbow.remove(0);
+		rainbow ~= RB_AMPL * sin(theta);
+		theta += TOTAL_ANGLE / NUM_RB_SEGS;
+	}
 }
 
 void render(ref State state) {
@@ -52,19 +69,19 @@ void render(ref State state) {
 	glTranslatef(0, -0.2, 0);
 	glScalef(1./state.width, 1./state.height, 1);
 
-    auto height = 100;
-    glColor3f(1.0, 0.0, 0.0);
-    fillRectTl(rainbow[0][0], rainbow[0][1] - height - height/2,
-               rainbow[1][0] - rainbow[0][0], height);
+	for (int i = 0; i < rainbow.length; i++) {
+    	glColor3f(1.0, 0.0, 0.0);
+    	fillRectTl(-1000 + i * SEG_W - 100, rainbow[i] - RB_HEIGHT * 1.5,
+    	           SEG_W, RB_HEIGHT);
 
-    glColor3f(0.0, 1.0, 0.0);
-    fillRectTl(rainbow[0][0], rainbow[0][1] - height/2,
-               rainbow[1][0] - rainbow[0][0], height);
+    	glColor3f(0.0, 1.0, 0.0);
+    	fillRectTl(-1000 + i * SEG_W - 100, rainbow[i] - RB_HEIGHT * 0.5,
+    	           SEG_W, RB_HEIGHT);
 
-    glColor3f(0.0, 0.0, 1.0);
-    fillRectTl(rainbow[0][0], rainbow[0][1] + height - height/2,
-               rainbow[1][0] - rainbow[0][0], height);
-
+    	glColor3f(0.0, 0.0, 1.0);
+    	fillRectTl(-1000 + i * SEG_W - 100, rainbow[i] - RB_HEIGHT * -0.5,
+    	           SEG_W, RB_HEIGHT);
+	}
 
 	// render the tetrahedron
 	glLoadIdentity();
